78b4731ef5bc5b6f71075c21d4f8c82fa7dff277
jmanning58 <manningjc@g.cofc.edu>
2021-03-07 21:32:24 -0500

    Add MainActivity Code

--- app/src/main/java/com/example/tipcalculator_manning/MainActivity.java
+++ app/src/main/java/com/example/tipcalculator_manning/MainActivity.java
@@ -1,14 +1,91 @@
 package com.example.tipcalculator_manning;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.view.View;
+import android.widget.Toast;
+import android.content.Intent;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final int REQUEST_CODE = 1;
+    private String TAG="MainActivity";
+    private Button calculateB;
+    private Button resetB;
+    private Intent i;
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.editText = findViewById(R.id.amount_line);
+
+        this.calculateB = findViewById(R.id.calculateButton);
+        i = new Intent(this, Calculate.class);
+
+        calculateB.setOnClickListener(v -> {
+            startActivity(i, REQUEST_CODE);
+        });
+
+        resetB.setOnClickListener(v -> {
+            
+        });
+    }
+
+    public void displayToast(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle saveInstanceState) {
+        super.onRestoreInstanceState(saveInstanceState);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "I entered onStart()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "I entered onResume()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "I entered onPause()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "I entered onStop()");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(TAG, "I entered onRestart()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "I entered onDestroy()");
     }
 }
